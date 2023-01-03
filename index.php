0e186c02113c138a2b2498ef567a51bf90148b97
Anthony <autosonic7@hotmail.com>
2023-01-02 19:49:28 -0800

    Beginning todo list

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=“en”>
+<head>
+<meta charset=“UTF-8”>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>My First Webpage</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="">
+</head>
+<body>
+
+<form action="newtodo.php" method="post">
+    <input type="text" name= "todo_name" placeholder= "Enter your todo">
+    <button>New task to do</button>
+<!--<h1>Hello World!</h1>
+<script src="" async defer></script>-->
+</body>
+</html>
+
---
+++ newtodo.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
+
+$todoName = $_POST['todo_name'] ?? '';
+$todoName = trim($todoName);
+
+if ($todoName) {
+    $json = file_get_contents('todo.json');
+    echo $json;
+//    file_put_contents('todo.json')
+}
